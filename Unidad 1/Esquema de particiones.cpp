91b8897c1cfd5ebbd6ec3c23ee2df9e93cbf520c
Edgar Diego <ing.diegovargas14@gmail.com>
2024-04-04 18:05:54 -0600

    Esquema de particiones 04-03-24

--- Unidad 1/Esquema de particiones.cpp
+++ Unidad 1/Esquema de particiones.cpp
@@ -10,11 +10,20 @@ void Crear_JT(void);
 void Imprimir_Tabla_Memoria(void);
 void Imprimir_Tabla_Tareas(void);
 
+void primerAjuste();
+void mejorAjuste();
+void peorAjuste();
+
+void Ordenamiento_Y_Ajuste(char);
+
 struct nodo_MT//Memory table
 {
-	int noBloque, capacidad, locIncial, locFinal, estado, acceso;// en nbloque poner B y el contador concatenado;
+	int noBloque, capacidad, locIncial, locFinal, estado, acceso, frag;// en nbloque poner B y el contador concatenado;
 	nodo_MT *sig;
 };
+
+void intercambiarNodos(nodo_MT *, nodo_MT *);
+int nBloques;
 
 struct nodo_JT//job table
 {
@@ -25,7 +34,7 @@ struct nodo_JT//job table
 
 //Aleatorio de 5 a 15 con 10 o 20 tareas
 
-nodo_MT *P_MT, *Q_MT, *Nuevo_MT, *Aux_MT;
+nodo_MT *P_MT, *Q_MT, *Nuevo_MT, *Aux_MT, *Aux2_MT, *Pivote;
 nodo_JT *P_JT, *Q_JT, *Nuevo_JT, *Aux_JT;
 
 int main()
@@ -34,6 +43,10 @@ int main()
 	Q_MT = NULL;
 	Crear_MT();
 	Crear_JT();
+	nBloques=Q_MT->noBloque;
+	//primerAjuste();
+	mejorAjuste();
+	//peorAjuste();
 	Imprimir_Tabla_Memoria();
 	Imprimir_Tabla_Tareas();
 	getch();
@@ -53,6 +66,7 @@ void Crear_MT(void)
 			P_MT -> locFinal = P_MT -> capacidad - 1;
 			P_MT -> estado = 0;//ocupado
 			P_MT -> acceso = NULL;
+			P_MT -> frag = 0;
 			
 			P_MT -> sig = NULL;
 			Q_MT = P_MT;
@@ -72,12 +86,13 @@ void Crear_MT(void)
 					Aux_MT=Aux_MT->sig;
 				}
 				
-				Nuevo_MT -> noBloque = cont+1;
+				Nuevo_MT -> noBloque = cont;
 				Nuevo_MT -> capacidad = maxMemoria-espacioOcupado;
 				Nuevo_MT -> locIncial = Q_MT->locFinal+1;
 				Nuevo_MT -> locFinal = maxMemoria;
 				Nuevo_MT -> estado = 1;
 				Nuevo_MT -> acceso = 0;
+				Nuevo_MT -> frag = 0;
 				
 				Nuevo_MT->sig=NULL;
 				Q_MT->sig=Nuevo_MT;
@@ -107,6 +122,7 @@ void Crear_MT(void)
 				Nuevo_MT -> locFinal = Nuevo_MT -> locIncial + espacioSolicitado*1024;
 				Nuevo_MT -> estado = 1;
 				Nuevo_MT -> acceso = 0;
+				Nuevo_MT -> frag = 0;
 				
 				Nuevo_MT->sig=NULL;
 				Q_MT->sig=Nuevo_MT;
@@ -128,7 +144,7 @@ void Crear_JT(void){
 		{
 			P_JT = (nodo_JT *) malloc(sizeof(nodo_JT));
 			P_JT -> ntarea = 1;
-			P_JT -> nespacio = 5+rand()%15;
+			P_JT -> nespacio = 1024*(5+rand()%15);
 			P_JT -> sig = NULL;
 			Q_JT = P_JT;
 		}
@@ -137,7 +153,7 @@ void Crear_JT(void){
 			Nuevo_JT = (nodo_JT *) malloc(sizeof(nodo_JT));
 			
 			Nuevo_JT -> ntarea = cont;
-			Nuevo_JT -> nespacio = 5+rand()%15;
+			Nuevo_JT -> nespacio = 1024*(5+rand()%15);
 
 			Nuevo_JT->sig=NULL;
 			Q_JT->sig=Nuevo_JT;
@@ -147,14 +163,149 @@ void Crear_JT(void){
 		i--;
 	}while(i>=0);
 }
+//nodo_MT *P_MT, *Q_MT, *Nuevo_MT, *Aux_MT;
+//nodo_JT *P_JT, *Q_JT, *Nuevo_JT, *Aux_JT;
 
+void intercambiarNodos(nodo_MT *A, nodo_MT *B){
+	nodo_MT *Temp;
+	Temp = (nodo_MT *) malloc(sizeof(nodo_MT));
+	//De A a Temp
+	Temp->acceso = A->acceso;Temp->capacidad = A->capacidad;Temp->estado = A->estado;
+	Temp->frag = A->frag;Temp->locFinal = A->locFinal;Temp->locIncial = A->locIncial;Temp->noBloque = A->noBloque;
+	//De B para A
+	A->acceso = B->acceso;A->capacidad = B->capacidad;A->estado = B->estado;
+	A->frag = B->frag;A->locFinal = B->locFinal;A->locIncial = B->locIncial;A->noBloque = B->noBloque;
+	//De Temp a B
+	B->acceso = Temp->acceso;B->capacidad = Temp->capacidad;B->estado = Temp->estado;
+	B->frag = Temp->frag;B->locFinal = Temp->locFinal;B->locIncial = Temp->locIncial;B->noBloque = Temp->noBloque;
+	free(Temp);//se elimina la memoria de Temp
+}
+
+void primerAjuste(){
+	Aux_JT=P_JT;
+	int bloquesDisponibles=nBloques;
+	Pivote = (nodo_MT *) malloc(sizeof(nodo_MT));
+	Pivote = P_MT;
+	while(bloquesDisponibles>0 && Aux_JT!=NULL){
+		Aux_MT=Pivote->sig;
+		while(Aux_MT!=Pivote){
+			if(Aux_JT->nespacio <= Aux_MT->capacidad && Aux_MT->estado==1){
+				Aux_MT->acceso = Aux_JT->ntarea;
+				Aux_MT->estado = 0;
+				Aux_MT->frag = Aux_MT->capacidad -  Aux_JT->nespacio;
+				bloquesDisponibles--;
+				Pivote = Aux_MT;
+				break;
+			}
+			if(Aux_MT==Q_MT){
+				Aux_MT=P_MT->sig;
+			}
+			else{
+				Aux_MT=Aux_MT->sig;
+			}
+		}
+		Aux_JT=Aux_JT->sig;
+	}
+
+}
+
+void mejorAjuste(){
+	Ordenamiento_Y_Ajuste('C');//Creciente
+}
+/*
+void mejorAjuste(void){
+	int particionesLibres = Q_MT -> noBloque;
+	int capUltimoBloque=0;
+	Aux_JT = P_JT;
+	Pivote = (nodo_MT *) malloc(sizeof(nodo_MT));
+	Aux_MT = P_MT -> sig;
+	Pivote = Aux_MT;
+	while(Aux_MT->sig!=NULL){
+		if(Aux_MT->capacidad < Pivote->capacidad && Aux_MT->estado == 0 && Aux_MT->capacidad > capUltimoBloque){
+			Pivote = Aux_MT;
+			capUltimoBloque = Aux_MT->capacidad;
+		}
+	}
+	
+	if(Aux_JT->nespacio <= Pivote->capacidad){
+		Pivote->acceso=Aux_JT->ntarea;
+		Pivote->estado=1;
+		Aux_JT=Aux_JT->sig;
+	}
+}
+*/
+void peorAjuste(){
+	Ordenamiento_Y_Ajuste('D');//Decreciente
+}
+void Ordenamiento_Y_Ajuste(char op){
+	int inter;
+	Aux2_MT = (nodo_MT *) malloc(sizeof(nodo_MT));
+	//Ordenamiento mediante el metodo de intercambio
+	do{ 
+		inter=0;
+		Aux_MT=P_MT->sig; 
+		while (Aux_MT->sig != NULL) {
+            Aux2_MT = Aux_MT->sig;
+            if (Aux_MT->capacidad  > Aux2_MT->capacidad && op=='C') {
+                // Intercambiar los valores de los nodos
+				intercambiarNodos(Aux_MT, Aux2_MT);
+                inter = 1; // Se realizó un intercambio, por lo tanto, la lista aún no está ordenada completamente
+            }
+            else if (Aux_MT->capacidad  < Aux2_MT->capacidad && op=='D') {
+                // Intercambiar los valores de los nodos
+				intercambiarNodos(Aux_MT, Aux2_MT);
+                inter = 1; // Se realizó un intercambio, por lo tanto, la lista aún no está ordenada completamente
+            }
+            Aux_MT = Aux2_MT;
+        }
+	}while (inter);
+	
+	Aux_JT=P_JT;
+	int bloquesDisponibles=nBloques;
+	while(bloquesDisponibles>0 && Aux_JT!=NULL){
+		Aux_MT=P_MT->sig;
+		while(Aux_MT!=NULL){
+			if(Aux_JT->nespacio <= Aux_MT->capacidad && Aux_MT->estado==1){
+				Aux_MT->acceso = Aux_JT->ntarea;
+				Aux_MT->estado = 0;
+				Aux_MT->frag = Aux_MT->capacidad -  Aux_JT->nespacio;
+				bloquesDisponibles--;
+				break;
+			}
+			Aux_MT=Aux_MT->sig;
+		}
+		Aux_JT=Aux_JT->sig;
+	}
+}
+/*
+//IMPRESION DE LA TABLA SIN EL ORDENAMIENTO INTERNO
+void Imprimir_Tabla_Memoria(void){
+	printf("\n\n--------------------------------------------------------------------------------------------\n|%-90s|\n--------------------------------------------------------------------------------------------","TABLA DE MEMORIA");
+	printf("\n|%-12s|%-12s|%-12s|%-12s|%-12s|%-12s|%-12s|", "NoBloque","Capacidad","locInicial","locFinal","Estado","Acceso","FI");
+	for(int i=0; i<=nBloques; i++){
+		Aux_MT=P_MT;
+		while(Aux_MT!=NULL){
+			if(i==Aux_MT->noBloque){
+				printf("\n--------------------------------------------------------------------------------------------");
+				printf("\n|B%-11d|%-6d bytes|%-12d|%-12d|%-12d|%-12d|%-12d|",Aux_MT->noBloque,Aux_MT->capacidad,Aux_MT->locIncial,Aux_MT->locFinal,Aux_MT->estado,Aux_MT->acceso,Aux_MT->frag);
+			}
+			Aux_MT=Aux_MT->sig;
+		}
+	}
+	printf("\n--------------------------------------------------------------------------------------------");
+	printf("%d",&nBloques);
+	
+}*/
+
+
+//IMPRESION DEL ORDENAMIENTO INTERNO DE LA TABLA 
 void Imprimir_Tabla_Memoria(void){
 	Aux_MT=P_MT;
 	printf("\n\n--------------------------------------------------------------------------------------------\n|%-90s|\n--------------------------------------------------------------------------------------------","TABLA DE MEMORIA");
 	printf("\n|%-12s|%-12s|%-12s|%-12s|%-12s|%-12s|%-12s|", "NoBloque","Capacidad","locInicial","locFinal","Estado","Acceso","FI");
 	while(Aux_MT!=NULL){
 		printf("\n--------------------------------------------------------------------------------------------");
-		printf("\n|B%-11d|%-6d bytes|%-12d|%-12d|%-12d|%-12d|%-12d|",Aux_MT->noBloque,Aux_MT->capacidad,Aux_MT->locIncial,Aux_MT->locFinal,Aux_MT->estado,Aux_MT->acceso,0);
+		printf("\n|B%-11d|%-6d bytes|%-12d|%-12d|%-12d|%-12d|%-12d|",Aux_MT->noBloque,Aux_MT->capacidad,Aux_MT->locIncial,Aux_MT->locFinal,Aux_MT->estado,Aux_MT->acceso,Aux_MT->frag);
 		Aux_MT=Aux_MT->sig;
 	}
 	printf("\n--------------------------------------------------------------------------------------------");
@@ -170,5 +321,8 @@ void Imprimir_Tabla_Tareas(void){
 		Aux_JT=Aux_JT->sig;
 	}
 	printf("\n-------------------------");
+	/*Aux_MT=P_MT;
+	while(Aux_MT->sig!=NULL){
+	printf("Dato %d, ",&P_MT->capacidad);
+	Aux_MT=Aux_MT->sig;}*/
 }
-
