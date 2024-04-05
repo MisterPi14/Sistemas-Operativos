11985250af1fa8500a5db2d23063155520befee9
Edgar Diego <ing.diegovargas14@gmail.com>
2024-04-04 18:04:50 -0600

    Esquema de particiones 26-02-24

--- Unidad 1/Esquema de particiones.cpp
+++ Unidad 1/Esquema de particiones.cpp
@@ -1,89 +1,165 @@
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
+//#define tamSistema 20000
+#define maxMemoria 80 * 1024
+#define tareas 20
 
-void PARTICIONADO(void);
-void CREAR_LISTA(void);
-void VER_LISTA(void);
+//void Particionado(void);
+void Crear_MT(void);
+void Crear_JT(void);
+void Imprimir_Tablas(void);
 
-struct nodo_lista
+struct nodo_MT//Memory table
 {
-	int noBloque, capacidad, locIncial, locFinal, estado, acceso;
-	nodo_lista *sig;
+	int noBloque, capacidad, locIncial, locFinal, estado, acceso;// en nbloque poner B y el contador concatenado;
+	nodo_MT *sig;
 };
 
-nodo_lista *Plista, *Qlista, *Nuevolista, *Aux;
+struct nodo_JT//job table
+{
+	int ntarea;//imprimir J y el numero contador concatenado
+	int nespacio;//aleatorio de 5 a 15 para las kb 5+rand()%15
+	nodo_JT *sig;
+};
+
+//Aleatorio de 5 a 15 con 10 o 20 tareas
+
+nodo_MT *P_MT, *Q_MT, *Nuevo_MT, *Aux_MT;
+nodo_JT *P_JT, *Q_JT, *Nuevo_JT, *Aux_JT;
 
 int main()
 {
-	Plista = NULL;
-	Qlista = NULL;
-	CREAR_LISTA();
-	VER_LISTA();
+	P_MT = NULL;
+	Q_MT = NULL;
+	Crear_MT();
+	Crear_JT();
+	Imprimir_Tablas();
 	getch();
 }
 
-void PARTICIONADO(void){
+/*void Particionado(void){
 	
-}
+}*/
 
-void CREAR_LISTA(void)
+void Crear_MT(void)
 {
-	int nuevo_elemento, r;
+	int espacioSolicitado, r=1, cont=1;
 	do
 	{
-		if (Plista == NULL)
+		if (P_MT == NULL)
 		{
-			Plista = (nodo_lista *) malloc(sizeof(nodo_lista));
-			Plista -> noBloque = 0;
-			Plista -> capacidad = 20*1024/*1kbytes*/;
-			Plista -> locIncial = 0;
-			Plista -> locFinal = Plista -> capacidad - 1;
-			Plista -> estado = 0;//ocupado
-			Plista -> acceso = NULL;
+			P_MT = (nodo_MT *) malloc(sizeof(nodo_MT));
+			P_MT -> noBloque = 0;
+			P_MT -> capacidad = 20*1024/*1kbytes*/;
+			P_MT -> locIncial = 0;
+			P_MT -> locFinal = P_MT -> capacidad - 1;
+			P_MT -> estado = 0;//ocupado
+			P_MT -> acceso = NULL;
 			
-			Plista -> sig = NULL;
-			Qlista = Plista;
+			P_MT -> sig = NULL;
+			Q_MT = P_MT;
 		}
 		else
 		{
-			Nuevolista = (nodo_lista *) malloc(sizeof(nodo_lista));
+			printf("\nDesea crear una nueva particion? (1=si 0=no): ");
+			scanf("%d",&r); if(r==0){break;}
 			
-			printf("Ingresa el bloque: ");
-			scanf ("%d", &nuevo_elemento);
-			Nuevolista -> noBloque = nuevo_elemento;
-			printf("Ingresa la capacidad (en kilobytes) de la 1er particion: ");
-			scanf ("%d", &nuevo_elemento);
-			Nuevolista -> capacidad = nuevo_elemento;
-			printf("Ingresa locIncial: ");
-			scanf ("%d", &nuevo_elemento);
-			Nuevolista -> locIncial = nuevo_elemento;
-			printf("Ingresa locFinal: ");
-			scanf ("%d", &nuevo_elemento);
-			Nuevolista -> locFinal = nuevo_elemento;
-			printf("Ingresa el estado: ");
-			scanf ("%d", &nuevo_elemento);
-			Nuevolista -> estado = nuevo_elemento;
-			printf("Ingresa acceso: ");
-			scanf ("%d", &nuevo_elemento);
-			Nuevolista -> acceso = nuevo_elemento;
+			Nuevo_MT = (nodo_MT *) malloc(sizeof(nodo_MT));
 			
-			Nuevolista->sig=NULL;
-			Qlista->sig=Nuevolista;
-			Qlista=Nuevolista;
+			printf("\nIngresa la capacidad (en kilobytes) de la particion %d: ",cont);
+			scanf ("%d", &espacioSolicitado);
+			
+			int espacioOcupado=0;
+			Aux_MT=P_MT;
+			while(Aux_MT!=NULL){
+				espacioOcupado+=Aux_MT->capacidad;
+				Aux_MT=Aux_MT->sig;
+			}
+			
+			if(maxMemoria-espacioOcupado>=espacioSolicitado*1024){
+				printf("\nParticion creada con exito, quedan: %d KiloBytes libres",maxMemoria-(espacioOcupado+espacioSolicitado*1024));
+				Nuevo_MT -> noBloque = cont;
+				Nuevo_MT -> capacidad = espacioSolicitado*1024;
+				Nuevo_MT -> locIncial = Q_MT->locFinal+1;
+				Nuevo_MT -> locFinal = Nuevo_MT -> locIncial + espacioSolicitado*1024;
+				Nuevo_MT -> estado = 1;
+				Nuevo_MT -> acceso = 0;
+				
+				Nuevo_MT->sig=NULL;
+				Q_MT->sig=Nuevo_MT;
+				Q_MT=Nuevo_MT;
+				cont++;
+			}
+			else{
+				printf("\nError: No se pudo crear la particion, quedan: %d KiloBytes libres",maxMemoria-(espacioOcupado+espacioSolicitado));
+			}
 		}
-	printf("\nDesea ingresar otro elemento (1=si/0=no)");
-	scanf("%d",&r);
 	}while(r==1);
 }
 
-void VER_LISTA(void){
-	Aux=Plista;
+void Crear_JT(void){
+	int espacioSolicitado, r=1, cont=1;
+	do
+	{
+		if (P_JT == NULL)
+		{
+			P_JT = (nodo_JT *) malloc(sizeof(nodo_JT));
+			P_JT -> noBloque = 0;
+			P_JT -> capacidad = 20*1024/*1kbytes*/;
+			P_JT -> locIncial = 0;
+			P_JT -> locFinal = P_JT -> capacidad - 1;
+			P_JT -> estado = 0;//ocupado
+			P_JT -> acceso = NULL;
+			
+			P_JT -> sig = NULL;
+			Q_JT = P_JT;
+		}
+		else
+		{
+			printf("\nDesea crear una nueva particion? (1=si 0=no): ");
+			scanf("%d",&r); if(r==0){break;}
+			
+			Nuevo_MT = (nodo_MT *) malloc(sizeof(nodo_MT));
+			
+			printf("\nIngresa la capacidad (en kilobytes) de la particion %d: ",cont);
+			scanf ("%d", &espacioSolicitado);
+			
+			int espacioOcupado=0;
+			Aux_MT=P_MT;
+			while(Aux_MT!=NULL){
+				espacioOcupado+=Aux_MT->capacidad;
+				Aux_MT=Aux_MT->sig;
+			}
+			
+			if(maxMemoria-espacioOcupado>=espacioSolicitado*1024){
+				printf("\nParticion creada con exito, quedan: %d KiloBytes libres",maxMemoria-(espacioOcupado+espacioSolicitado*1024));
+				Nuevo_MT -> noBloque = cont;
+				Nuevo_MT -> capacidad = espacioSolicitado*1024;
+				Nuevo_MT -> locIncial = Q_MT->locFinal+1;
+				Nuevo_MT -> locFinal = Nuevo_MT -> locIncial + espacioSolicitado*1024;
+				Nuevo_MT -> estado = 1;
+				Nuevo_MT -> acceso = 0;
+				
+				Nuevo_MT->sig=NULL;
+				Q_MT->sig=Nuevo_MT;
+				Q_MT=Nuevo_MT;
+				cont++;
+			}
+			else{
+				printf("\nError: No se pudo crear la particion, quedan: %d KiloBytes libres",maxMemoria-(espacioOcupado+espacioSolicitado));
+			}
+		}
+	}while(r==1);
+}
+
+void Imprimir_Tablas(void){
+	Aux_MT=P_MT;
 	printf("\n|%-10s| |%-10s| |%-10s| |%-10s| |%-10s| |%-10s|", "NoBloque","Capacidad","locInicial","locFinal","Estado","Acceso");
-	while(Aux!=NULL){
+	while(Aux_MT!=NULL){
 		printf("\n-----------------------------------------------------------------------------");
-		printf("\n|%-10d| |%-10d| |%-10d| |%-10d| |%-10d| |%-10d|",Aux->noBloque,Aux->capacidad,Aux->locIncial,Aux->locFinal,Aux->estado,Aux->acceso);
-		Aux=Aux->sig;
+		printf("\n|B%-10d| |%-8dBytes| |%-8dBytes| |%-8dBytes| |%-10d| |%-10d|",Aux_MT->noBloque,Aux_MT->capacidad,Aux_MT->locIncial,Aux_MT->locFinal,Aux_MT->estado,Aux_MT->acceso);
+		Aux_MT=Aux_MT->sig;
 	}	
 }
 
